fbe160f14de8a99baa5793c21dccaf6fd8b2fcfb
xSharkz <105187265+xSharkz@users.noreply.github.com>
2024-11-27 19:28:42 -0300

    feat: add app components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RickAndMortyService } from './services/rick-and-morty.service';
+import { CharacterListComponent } from './components/character-list/character-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, CharacterListComponent],
 })
-export class AppComponent {
-  title = 'prueba-corta2-idwm';
+export class AppComponent implements OnInit {
+  characters: any[] = [];
+  nextPage: string | null = null;
+  prevPage: string | null = null;
+
+  constructor(private rickAndMortyService: RickAndMortyService) {}
+
+  ngOnInit() {
+    this.loadCharacters();
+  }
+
+  loadCharacters(page: number = 1) {
+    this.rickAndMortyService.getCharacters(page).subscribe(
+      (data: any) => {
+        this.characters = data.results;
+        this.nextPage = data.info.next;
+        this.prevPage = data.info.prev;
+      },
+      (error) => {
+        console.error('Error al cargar los personajes', error);
+      }
+    );
+  }
+
+  goToNextPage() {
+    if (this.nextPage) {
+      const page = this.extractPageNumber(this.nextPage);
+      this.loadCharacters(page);
+    }
+  }
+
+  goToPreviousPage() {
+    if (this.prevPage) {
+      const page = this.extractPageNumber(this.prevPage);
+      this.loadCharacters(page);
+    }
+  }
+
+  extractPageNumber(url: string): number {
+    const match = url.match(/page=(\d+)/);
+    return match ? parseInt(match[1], 10) : 1;
+  }
 }
